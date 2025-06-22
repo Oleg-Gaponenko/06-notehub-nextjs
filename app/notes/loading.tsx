5946cba3f8214782031c9f953ae9ed8fa37a5a32
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-06-23 00:06:16 +0300

    add Loader to loading in app file

--- app/notes/loading.tsx
+++
@@ -1,9 +0,0 @@
-import css from './Loading.module.css';
-
-export default function Loading() {
-  return (
-    <div className={css.center}>
-      <p className={css.loading}>Loading, please wait...</p>
-    </div>
-  );
-}
