56c26f56733516c36aade7d84d94779835e931eb
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-06-22 23:40:49 +0300

    add promise into params for NoteDetails

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,20 +4,19 @@ import getQueryClient from '@/lib/getQueryClient';
 import NoteDetails from './NoteDetails.client';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
-interface PageProps {
-  params: {
-    id: string;
-  };
+interface NoteDetailsProps {
+  params: Promise<{ id: string }>;
 }
 
-export default async function NoteDetailsPage({ params }: PageProps) {
-  const id = Number(params.id);
+export default async function NoteDetailsPage({ params }: NoteDetailsProps) {
+  const { id } = await params;
+  const noteId = Number(id);
 
   const queryClient = getQueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', id],
-    queryFn: () => fetchNoteById(id),
+    queryKey: ['note', noteId],
+    queryFn: () => fetchNoteById(noteId),
   });
 
   const dehydratedState = dehydrate(queryClient);
